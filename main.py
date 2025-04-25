ff351a01ad1eab126fe34906a6c98e9a32d2c631
MireauLijbaert <mireau.lijbaert@student.howest.be>
2025-04-25 21:24:11 +0200

    didn't add Functions. in front whoops

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from Screens.Login_Screen import LoginScreen  # import from your folder
 import Functions.sqlConnectionHelper
 
-if sqlConnectionHelper.TestConnection():
+if Functions.sqlConnectionHelper.TestConnection():
       print("✅ MySQL is connected and ready!")
 else:
     print("❌ MySQL connection failed.")
